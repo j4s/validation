f899abae02615262c8861c248405837e899f36e7
insolution4 <soloscriptura@mail.ru>
2018-12-07 19:06:38 +0300

    close #39, close #32, close #34

--- tests/FiltrationFlagsTest.php
+++ tests/FiltrationFlagsTest.php
@@ -6,11 +6,11 @@ declare(strict_types=1);
 namespace j4s\validation;
 
 /**
- * Class FiltrationFlagsTest - Тесты для класса FiltrationFlags
+ * Тесты для класса FiltrationFlags
  *
  * @package     superglobals
  * @author      Eugeniy Makarkin <soloscriptura@mail.ru>
- * @version     v1.0.0 2018-11-27 11:00:22
+ * @version     v1.0.1 2018-12-06 15:35:09
  * @todo Проверить комментарии phpDocumentor!!
  */
 class FiltrationFlagsTest
@@ -31,12 +31,13 @@ class FiltrationFlagsTest
 
     /**
      * Тест для метода int
-     * @version v1.0.0 2018-11-27 11:01:31
+     * @version v1.0.1 2018-12-06 15:34:39
+     * @global object $UTest - Глобальный объект UTest
      * @return string - html тег с сообщением результата прохождения теста
      */
     public static function intTest()
     {
-        global $UTest, $RE1;
+        global $UTest;
 
         $UTest->methodName = 'int';
 
@@ -45,8 +46,8 @@ class FiltrationFlagsTest
         $UTest->nextHint = 'Пустое значение';
         $expect = 0;
         // Act
-        $RE1 = new FiltrationFlags('');
-        $act = $RE1->int();
+        $Filtration = new FiltrationFlags('');
+        $act = $Filtration->int();
         // Assert Test
         $UTest->isEqual("int();", $expect, $act);
 
@@ -55,8 +56,8 @@ class FiltrationFlagsTest
         $UTest->nextHint = 'Не цифра';
         $expect = 0;
         // Act
-        $RE1 = new FiltrationFlags('onlykey');
-        $act = $RE1->int();
+        $Filtration = new FiltrationFlags('onlykey');
+        $act = $Filtration->int();
         // Assert Test
         $UTest->isEqual("int();", $expect, $act);
 
@@ -65,8 +66,8 @@ class FiltrationFlagsTest
         $UTest->nextHint = 'Цифра';
         $expect = 123;
         // Act
-        $RE1 = new FiltrationFlags(123);
-        $act = $RE1->int();
+        $Filtration = new FiltrationFlags(123);
+        $act = $Filtration->int();
         // Assert Test
         $UTest->isEqual("int();", $expect, $act);
 
